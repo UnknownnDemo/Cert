87183ea344ab36da135be47d952b3bf50dc87249
Keerthana0004 <keerthanasivan04@gmail.com>
2025-02-17 22:36:27 +0530

    issue page

--- web-app/src/components/CenteredBox/CenteredBox.jsx
+++ web-app/src/components/CenteredBox/CenteredBox.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './CenteredBox.css';
 export const CenteredBox = () => {
   return (
@@ -6,7 +7,9 @@ export const CenteredBox = () => {
       <div className="box">
         <p>A Decentralized Certificate Issuance and Verification System.</p>
         <div className="buttonContainer">
+        <Link to="/issue">
           <button className="actionButton">Issue</button>
+          </Link>
           <button className="actionButton">Verify</button>
         </div>
       </div>
